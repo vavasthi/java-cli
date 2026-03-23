0b8387ea984ba054e1e9b7c86a57faf9cc5d9659
Vinay Avasthi <vinay@avasthi.com>
2026-03-23 20:14:08 +0530

    Pushing changes

--- src/main/java/org/avasthi/java/cli/DumpStraddleSimulationResults.java
+++ src/main/java/org/avasthi/java/cli/DumpStraddleSimulationResults.java
@@ -18,6 +18,9 @@ import java.util.*;
 
 public class DumpStraddleSimulationResults extends Base {
 
+  record ProfitMilestones(Date threePercent, Date sixPercent, Date tenPercent, Date fifteenPercent, Date twentyPercent) {
+
+  }
   public static void main(String[] args) throws IOException, InterruptedException, ParseException, CsvException {
     DumpStraddleSimulationResults lqr = new DumpStraddleSimulationResults();
     lqr.dumpSimulationResults();
@@ -25,16 +28,23 @@ public class DumpStraddleSimulationResults extends Base {
   private void dumpSimulationResults() throws FileNotFoundException {
 
     MongoCollection<SimulatedLongStraddle> simulatedLongStraddleCollection = getMongoClient().getDatabase(database).getCollection(simulatedTradeName, SimulatedLongStraddle.class);
-    String header = "id,timestamp,asset,status,strike,spotPrice,profitOpportunities,maxProfit,maxLoss,callSymbol,putSymbol,vix,callBuyIv,putBuyIv,callSellIv,putSellIv,callQuantity,putQuantity," +
+    String header = "id,timestamp,3pts,6pts,10pts,15pts,20pts,asset,status,strike,spotPrice,profitOpportunities,maxProfit,maxLoss,callSymbol,putSymbol,vix,callBuyIv,putBuyIv,callSellIv,putSellIv,callQuantity,putQuantity," +
             "callBuyPremium,putBuyPremium,callSellPremium,putSellPremium,callBuyVolume,putBuyVolume,callSellVolume,putSellVolume,callBuyTimestamp,putBuyTimestamp,callSellTimestamp, putSellTimestamp," +
             "callBuyOI,putBuyOI,callSellOI,putSellOI";
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     try (final PrintWriter pw = new PrintWriter("dump.csv");) {
       pw.println(header);
       simulatedLongStraddleCollection.find().forEach(s -> {
-        String line = String.format("%s,%s,%s,%s,%.2f,%.2f,%d,%.2f,%.2f,%s,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d,%.2f,%.2f,%.2f,%.2f,%d,%d,%d,%d,%s,%s,%s,%s,%.2f,%.2f,%.2f,%.2f",
+
+        ProfitMilestones profitMilestones = computeProfitMilestones(s);
+        String line = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%.2f,%.2f,%d,%.2f,%.2f,%s,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d,%.2f,%.2f,%.2f,%.2f,%d,%d,%d,%d,%s,%s,%s,%s,%.2f,%.2f,%.2f,%.2f",
                 s.getTradeId(),
                 sdf.format(s.getTimestamp()),
+                profitMilestones != null && profitMilestones.threePercent() != null ? sdf.format(profitMilestones.threePercent()) : "",
+                profitMilestones != null && profitMilestones.sixPercent() != null ? sdf.format(profitMilestones.sixPercent()) : "",
+                profitMilestones != null && profitMilestones.tenPercent() != null ? sdf.format(profitMilestones.tenPercent()) : "",
+                profitMilestones != null && profitMilestones.fifteenPercent() != null ? sdf.format(profitMilestones.fifteenPercent()) : "",
+                profitMilestones != null && profitMilestones.twentyPercent() != null ? sdf.format(profitMilestones.twentyPercent()) : "",
                 s.getAsset(),
                 s.getSell() == null ? "Open" : "Closed",
                 s.getStrike(),
@@ -73,5 +83,36 @@ public class DumpStraddleSimulationResults extends Base {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+  private ProfitMilestones computeProfitMilestones(SimulatedLongStraddle trade) {
+    if (trade.getOtherSellOpportunities() == null || trade.getOtherSellOpportunities().isEmpty()) {
+      return null;
+    }
+    Date threePercent = null;
+    Date sixPercent = null;
+    Date tenPercent = null;
+    Date fifteenPercent = null;
+    Date twentyPercent = null;
+    double cost = trade.getBuy().call().getPremium() * trade.getBuy().call().getQuantity() + trade.getBuy().put().getPremium() * trade.getBuy().put().getQuantity();
+    for (SimulatedLongStraddle.Straddle oso : trade.getOtherSellOpportunities()) {
+      double currentValue = oso.call().getPremium() * oso.call().getQuantity() + oso.put().getPremium() * oso.put().getQuantity();
+      double percentageProfit = (currentValue - cost) / cost;
+      if (percentageProfit > 0.20 && twentyPercent == null) {
+        twentyPercent = oso.call().getTimestamp();
+      }
+      else if (percentageProfit > 0.15 && fifteenPercent == null) {
+        fifteenPercent = oso.call().getTimestamp();
+      }
+      else if (percentageProfit > 0.10 && tenPercent == null) {
+        tenPercent = oso.call().getTimestamp();
+      }
+      else if (percentageProfit > 0.06 && sixPercent == null) {
+        sixPercent = oso.call().getTimestamp();
+      }
+      else if (percentageProfit > 0.03  && threePercent == null) {
+        threePercent = oso.call().getTimestamp();
+      }
+    }
+    return new ProfitMilestones(threePercent, sixPercent, tenPercent, fifteenPercent, twentyPercent);
   }
 }
